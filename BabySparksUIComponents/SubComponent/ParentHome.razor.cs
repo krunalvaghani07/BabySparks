aced9ba7a46949bb821d144a16b23b3bfdb0533d
krunal <krunalvaghani07.com>
2024-06-13 13:06:55 -0400

    WIP Home

--- BabySparksUIComponents/SubComponent/ParentHome.razor.cs
+++ BabySparksUIComponents/SubComponent/ParentHome.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Radzen;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,10 @@ namespace BabySparksUIComponents.SubComponent
 {
     public partial class ParentHome : ComponentBase
     {
-
+        TabPosition tabPosition = TabPosition.Top;
+        void OnChange(int index)
+        {
+            Console.WriteLine($"Tab with index {index} was selected.");
+        }
     }
 }
